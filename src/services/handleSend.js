5479fdeb311fab2aac52e59d77f3e9b077dc353c
PARK-SSOYEON <soyeon00307@gmail.com>
2024-08-23 14:14:14 +0900

    feat: 채팅 DB에서 불러오기 구현

--- src/services/handleSend.js
+++ src/services/handleSend.js
@@ -2,23 +2,26 @@ import apiClient from "./apiClient.js";
 
 export const handleSend = async (message, setChatMessages)  => {
     if (message.trim()) {
+        const timestamp = new Date();
         setChatMessages((prevMessages) => [
             ...prevMessages,
-            {text: message, sender:'user'}
+            {text: message, sender:'user', timestamp}
         ]);
 
         try {
             const response = await apiClient.post('/chat', {message});
-
+            const botTimestamp = new Date();
             setChatMessages((prevMessages) => [
                 ...prevMessages,
-                {text: response.data.message, sender: 'bot'},
+                {text: response.data.message, sender: 'bot', timestamp: botTimestamp},
             ]);
         } catch (error) {
             console.error('Error:', error);
+
+            const errorTimestamp = new Date();
             setChatMessages((prevMessages) => [
                 ...prevMessages,
-                {text: 'Error in getting response from server', sender: 'bot'},
+                {text: 'Error in getting response from server', sender: 'bot', timestamp: errorTimestamp},
             ]);
         }
     }
